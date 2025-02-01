91983b187f1e1fee14289a11eabef6cd88631afc
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-01 13:20:08 -0300

    Finaliza tipos de referencia

---
+++ TiposDeDados4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Definindo variaveis do tipo string, object e dynamic");
+Console.WriteLine();
+
+string nome = "Maria";
+String titulo = "Programador C#";
+
+Console.WriteLine(nome);
+Console.WriteLine(titulo);
+
+string valor = "Isso é uma string";
+
+valor = "Isso é uma string alterada";
+
+Console.WriteLine(valor);
+
+Object nota = 10;
+Object valor1 = 8.55m;
+Object nome1 = "Maria";
+Object ativa = true;
+Object letra = 'A';
+
+Console.WriteLine(nota);
+Console.WriteLine(valor1);
+Console.WriteLine(nome1);
+Console.WriteLine(ativa);
+Console.WriteLine(letra);
