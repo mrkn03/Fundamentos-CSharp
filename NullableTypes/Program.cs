168178e5ecd884abdf3ed0ae062345dda9c4c0ac
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-01 22:36:49 -0300

    Finaliza nullable types"

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("## Nullable Types ##\n");
+
+int? i = null;
+double? j = null;
+bool? k = null;
+
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+
+int? a = null;
+int b = a ?? 0;
+
+Console.WriteLine(b);
+
+int? x = 4;
+int? y = 3;
+int? z = x * y;
+
+Console.WriteLine(z);
+
+int? c = null;
+
+if (c.HasValue)
+{
+    Console.WriteLine(c.Value);
+}
+else
+{
+    Console.WriteLine("c não possui um valor");
+}
