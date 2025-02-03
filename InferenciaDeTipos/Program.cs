a5364670ad1e27b830155b6e0355e76e86d79fb5
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-02 23:07:21 -0300

    Inferencia de Tipos

---
+++ InferenciaDeTipos/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("## Inferencia de Tipos ##\n");
+
+var idade = 25;
+var nome = "Maria";
+var salario = 2500.00m;
+
+Console.WriteLine($"{nome} tem {idade} anos e recebe R${salario}");
+
+////Limitacoes var
+//var salario = null; //Erro
+//var titulo; //Erro
+//var salario, imposto, total; //Erro
+
+//Nao posso mudar o tipo apos inicializar
+var num = 10;
+num += 20;
+//num = "teste"; //Erro
+
