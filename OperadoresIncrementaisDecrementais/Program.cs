b0c2ea94f41ec7e23c95fc7b36346615dd247db1
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-03 20:16:59 -0300

    Operadores de Incremento e Decremento

---
+++ OperadoresIncrementaisDecrementais/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("## Operadores de Incremento e Decremento ##\n");
+
+int x = 1;
+x++;
+Console.WriteLine(x);
+x--;
+Console.WriteLine(x);
+
+
+//pós incremento
+int y = 0;
+int resultado = y++ + 10;
+Console.WriteLine($"pós incremento -> {resultado}");
+Console.WriteLine($"Valor de y = {y}");
+
+//pré incremento
+int z = 0;
+int resultado2 = ++z + 10;
+Console.WriteLine($"pré incremento -> {resultado2}");
+Console.WriteLine($"Valor de y = {z}");
