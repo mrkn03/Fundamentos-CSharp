ef023c5d7c17e5c082d1b4890dbd5e9e8d15bd3a
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-02 22:55:39 -0300

    Operadores aritmeticos

--- EntradaDeDados/Program.cs
+++ EntradaDeDados/Program.cs
@@ -3,10 +3,9 @@
 Console.WriteLine("Digite seu nome: ");
 string nome = Console.ReadLine();
 
-
-Console.WriteLine($"O nome digitado foi: {nome}");
-
-Console.WriteLine("Digite sua idade: ");
+Console.WriteLine("\nDigite sua idade: ");
 int idade = Convert.ToInt32(Console.ReadLine());
 
+
+Console.WriteLine($"O nome digitado foi: {nome}"); 
 Console.WriteLine($"A idade digitada foi: {idade}");
---
+++ OperadoresAritmeticos/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("## Operadores Aritimeticos ##\n");
+
+Console.Write("Informe o valor de x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Informe o valor de y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"soma de x + y = {x + y}");
+Console.WriteLine($"subtraçao de x - y = {x - y}");
+Console.WriteLine($"multiplicacao de x * y = {x * y}");
+double divisao = (double)x / y;
+Console.WriteLine($"divisao de x / y = {divisao}");
+Console.WriteLine($"modulo de x % y = {x % y}");
+
+//Usando classe Math
+Console.WriteLine($"Raiz quadrada de x = {Math.Sqrt(x)}");
+Console.WriteLine($"potencia de x elevado a y = {Math.Pow(x, y)}");
+Console.WriteLine($"valor maximo entre x e y = {Math.Max(x, y)}");
+Console.WriteLine($"valor minimo entre x e y = {Math.Min(x, y)}");
+Console.WriteLine($"coseno de x = {Math.Cos(x)}");
+Console.WriteLine($"seno de x = {Math.Sin(x)}");
+Console.WriteLine($"exponencial de x = {Math.Exp(x)}");
