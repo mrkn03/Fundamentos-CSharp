9f1c4b1e4fa6a0f953a861fe221ad16681b14afa
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-03 20:24:19 -0300

    Nullable Types

--- NullableTypes/Program.cs
+++ NullableTypes/Program.cs
@@ -1,31 +1,61 @@
 ﻿Console.WriteLine("## Nullable Types ##\n");
 
-int? i = null;
-double? j = null;
-bool? k = null;
+Nullable<int> i = null;
+Nullable<bool> b = null;
+Nullable<double> d = null;
+Nullable<float> f = null;
 
-Console.WriteLine(i);
-Console.WriteLine(j);
-Console.WriteLine(k);
+Console.WriteLine($"i={i}");
+Console.WriteLine($"b={b}");
+Console.WriteLine($"d={d}");
+Console.WriteLine($"f={f}");
 
-int? a = null;
-int b = a ?? 0;
+Console.ReadKey();
 
-Console.WriteLine(b);
+int? vari = null;
+bool? varb = null;
+double? vard = null;
+float? varf = null;
+
+Console.WriteLine($"i={vari}");
+Console.WriteLine($"b={varb}");
+Console.WriteLine($"d={vard}");
+Console.WriteLine($"f={varf}");
+
+Console.ReadKey();
+
+int a1 = 100;
+int? b1 = a1;
+Console.WriteLine(b1);
+Console.ReadKey();
+
+int? a2 = null;
+int b2 = a2 ?? 0;
+Console.WriteLine(b2);
+
+Console.ReadKey();
 
 int? x = 4;
-int? y = 3;
-int? z = x * y;
-
+int y = 3;
+//int z = x * y;
 Console.WriteLine(z);
+Console.ReadLine();
 
-int? c = null;
+int? x1 = 4;
+int? y1 = 3;
+int? z1 = x1 * y1;
+Console.WriteLine(z1);
 
-if (c.HasValue)
+Console.ReadKey();
+
+int? b3 = 100;
+if (b.HasValue)
 {
-    Console.WriteLine(c.Value);
+    Console.WriteLine($"b3 is {b3.Value}");
 }
 else
 {
-    Console.WriteLine("c não possui um valor");
+    Console.WriteLine("b3 does not have a value");
 }
+
+Console.ReadKey();
