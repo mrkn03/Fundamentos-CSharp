3b0fa46117c9452026190128bdb461c28ab3911b
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-05 22:30:49 -0300

    Classe e Objeto

---
+++ ClassesMetodos/ClassesMetodos/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("## Classes e Métodos ##\n");
+
+Pessoa p1 = new Pessoa();
+p1.nome = "João";
+p1.idade = 20;
+p1.sexo = "M";
+
+Pessoa p2 = new Pessoa();
+p2.nome = "Maria";
+p2.idade = 30;
+p2.sexo = "F";
+
+class Pessoa
+{
+    public string? nome;
+    public int idade;
+    public string? sexo;
+
+
+}
