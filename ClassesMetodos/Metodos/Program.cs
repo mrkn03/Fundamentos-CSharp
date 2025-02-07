a0460706930a7834c54e8df434c15523acf65380
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-06 21:05:14 -0300

    Metodos

---
+++ ClassesMetodos/Metodos/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("## Metodos ##\n");
+
+MinhaClasse minhaClasse = new MinhaClasse();
+
+minhaClasse.saudacao();
+
+class MinhaClasse
+{
+    public void saudacao() {
+        Console.WriteLine("Bem Vindo");
+        exibirDataAtual();
+    }
+
+    public void exibirDataAtual()
+    {
+        Console.WriteLine(DateTime.Now);
+    }
+}
