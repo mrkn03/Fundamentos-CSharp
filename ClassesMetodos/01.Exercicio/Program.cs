07d8201fa5c788e38eac99f98de38030eb4edc11
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-08 23:08:10 -0300

    \

---
+++ ClassesMetodos/01.Exercicio/Program.cs
@@ -0,0 +1,42 @@
+﻿
+Carro chevrolet = new Carro("Sedan", "Chevrolet", "Onix", 2016, 110);
+
+chevrolet.Acelerar(chevrolet.marca);
+Console.WriteLine($"{chevrolet.modelo} {chevrolet.montadora} {chevrolet.marca}" +
+
+$" {chevrolet.ano} {chevrolet.potencia} CV");
+
+Carro ford = new Carro("SUV", "Ford", "EcoSport", 2018, 120);
+
+ford.Acelerar(ford.marca);
+
+Console.WriteLine($"{ford.modelo} {ford.montadora} {ford.marca}" +
+$" {ford.ano} {ford.potencia} CV");
+
+public class Carro
+{
+    public string? modelo;
+    public string? montadora;
+    public string? marca;
+    public int ano;
+    public double potencia;
+
+    public Carro()
+    {}
+
+    public Carro(string modelo, string montadora, string marca, int ano, double potencia)
+    {
+        this.modelo = modelo;
+        this.montadora = montadora;
+        this.marca = marca;
+        this.ano = ano;
+        this.potencia = potencia;
+    }
+
+    public void Acelerar(string marca)
+    {
+        Console.WriteLine($"Acelerando o meu {marca}");
+    }
+
+    
+}
--- ClassesMetodos/Construtor/Program.cs
+++ ClassesMetodos/Construtor/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("## Construtor ##\n");
+
+
+Aluno aluno = new Aluno("Maria", 22, "Feminino", "Aprovado");
+
+
+//operador (condicao ? valor1 : valor2)
+Console.WriteLine(aluno.Nome == null ? "null" : aluno.Nome);
+Console.WriteLine(aluno.Idade == 0 ? "zero" : aluno.Idade);
+Console.WriteLine(aluno.Sexo == null ? "null" : aluno.Sexo);
+Console.WriteLine(aluno.Aprovado == null ? "null" : aluno.Aprovado);
+
+
+public class Aluno
+{
+    public string? Nome;
+    public int Idade;
+    public string? Sexo;
+    public string? Aprovado;
+
+    public Aluno(string nome) => Nome = nome;
+
+
+    public Aluno(string nome, int idade, string sexo, string aprovado) : this(nome)
+    {
+        Idade = idade;
+        Sexo = sexo;
+        Aprovado = aprovado;
+    }
+}
