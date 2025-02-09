9334368534493c4e9df3dce01ba242b1d2b33ede
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-09 12:23:04 -0300

    Exemplo Pratico - Metodos

---
+++ ClassesMetodos/ExemploPratico-Metodos/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("## Exemplo Pratico - Metodos ##\n");
+
+Cadastro cadastro = new();
+var cliente = cadastro.Registrar();
+cadastro.ExibirDados(cliente);
+
+//alterar renda
+cliente = cadastro.Registrar(cliente);
+cadastro.ExibirDados("Renda alterada", cliente);
+
+public class Cliente
+{
+    public string? Nome;
+    public int Idade;
+    public decimal Renda;
+
+    public Cliente()
+    { }
+
+    public Cliente(string? nome, int idade, decimal renda)
+    {
+        Nome = nome;
+        Idade = idade;
+        Renda = renda;
+    }
+
+
+}
+
+public class Cadastro
+{
+    public Cliente Registrar()
+    {
+        Cliente cliente = new("Maria", 23, 3000);
+        return cliente;
+    }
+
+    public Cliente Registrar(Cliente cliente)
+    {
+        cliente.Renda = 3500;
+        return cliente;
+    }
+
+    public void ExibirDados(Cliente cliente)
+    {
+        Console.WriteLine($"{cliente.Nome} {cliente.Idade} {cliente.Renda.ToString("c")}");
+    }
+
+    public void ExibirDados(string texto, Cliente cliente)
+    {
+        Console.Write($"\n{texto}\t");
+        Console.Write($" {cliente.Nome} {cliente.Renda.ToString("c")}");
+    }
+}
