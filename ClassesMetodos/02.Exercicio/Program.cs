3c82b500e9243d66d30ebba6e416806b9d29d178
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-09 01:06:08 -0300

    Exercicio 02

---
+++ ClassesMetodos/02.Exercicio/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Carro
+{
+    public string? modelo;
+    public string? montadora;
+    public string? marca;
+    public int ano;
+    public double potencia;
+
+    public Carro()
+    { }
+
+    public Carro(string? modelo, string? montadora)
+    {
+        this.modelo = modelo;
+        this.montadora = montadora;
+    }
+
+    public Carro(string modelo, string montadora, string marca, int ano, double potencia)
+    {
+        this.modelo = modelo;
+        this.montadora = montadora;
+        this.marca = marca;
+        this.ano = ano;
+        this.potencia = potencia;
+    }
+
+    public void Acelerar(string? marca)
+    {
+        Console.WriteLine($"Acelerando o meu {marca}");
+    }
+
+    public double VelocidadeMaxima(int potencia)
+    {
+        return potencia * 1.75;
+    }
+
+}
