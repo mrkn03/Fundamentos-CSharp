5cd28458ac363581b82fce3d159f08bc30afb6f2
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-09 17:49:29 -0300

    Construtor estatico

---
+++ ClassesMetodos/ConstrutorEstatico/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿public class Pessoa
+{
+    public static int IdadeMinima;
+
+    public int Idade { get; set; }
+    public string Nome { get; set; } = string.Empty;
+
+    public Pessoa()
+    {}
+    public Pessoa(int idade, string nome)
+    {
+        Console.WriteLine("\nExecutando o construtor parametrizado");
+        Idade = idade;
+        Nome = nome;
+    }
+
+
+    static Pessoa()
+    {
+        Console.WriteLine("\nExecutando o construtor estatico");
+        Console.WriteLine("\nInicializando o campo IdadeMinima");
+        IdadeMinima = 18;
+    }
+}
