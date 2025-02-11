499f3a5a786d2e2d81140c3f1b6dd8875dfad518
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-10 21:13:59 -0300

    Exercicio 08

---
+++ ClassesMetodos/08.Exercicio/Program.cs
@@ -0,0 +1,45 @@
+﻿Cliente cliente = new("Maria", "maria@email.com", 19);
+Cliente.ExibirInfo(email: cliente.Email, nome: cliente.Nome, idade: cliente.Idade);
+Console.WriteLine("\nExibindo informação sem informar a idade");
+Cliente.ExibirInfo(email: cliente.Email, nome: cliente.Nome);
+
+public struct Cliente
+{
+    public string? Nome { get; set; }
+    public string? Email { get; set; }
+    private int idade;
+    public int Idade
+    {
+        get { return idade; }
+        set
+        {
+            if (value < 18)
+            {
+                idade = 18;
+                Console.WriteLine("Cliente menor que 18 anos");
+            }
+            else
+            {
+                Console.WriteLine("Cliente maior que 18 anos");
+                idade = value;
+            }
+        }
+    }
+
+    public Cliente()
+    {
+        
+    }
+
+    public Cliente(string nome, string email, int idade)
+    {
+        Nome = nome;
+        Email = email;
+        Idade = idade;
+    }
+    public static void ExibirInfo(string nome, string email, int idade = 18)
+    {
+        Console.WriteLine($"{nome} - {email} - {idade}");
+    }
+
+}
