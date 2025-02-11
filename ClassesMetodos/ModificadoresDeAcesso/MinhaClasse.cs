015bcc4725f2513958b4f1e674b1d6ab9652cbdd
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-10 22:42:38 -0300

    Modificadores de Acesso

---
+++ ClassesMetodos/ModificadoresDeAcesso/MinhaClasse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadoresDeAcesso
+{
+    internal class MinhaClasse
+    {
+        int MeuCampo;
+
+        string? MinhaPropriedade { get; set; }
+
+        void MeuMetodo()
+        {
+            Console.WriteLine("Meu Metodo");
+        }
+    }
+}
