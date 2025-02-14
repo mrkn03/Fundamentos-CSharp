5c94da0676d4ec817be6018f84743995fc888114
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-13 21:13:41 -0300

    Exercicio - Arrays

--- Colecoes/Exercicio-Arrays/Program.cs
+++ Colecoes/Exercicio-Arrays/Program.cs
@@ -1,17 +1,24 @@
-﻿string[] alunos = new string[5];
+﻿string?[] alunos = new string[5];
 double[] notas = new double[5];
 
-for(int i = 0; i< alunos.Length; i++)
+for (int i = 0; i < alunos.Length; i++)
 {
-    Console.Write($"Digite o nome do {i+1} aluno: ");
-    alunos[i] = Console.ReadLine();
-    Console.Write($"Digite a nota do {i+1} aluno: ");
-    notas[i] = Convert.ToDouble(Console.ReadLine());
+    Console.Write($"Digite o nome do {i + 1} aluno: ");
+    string? aluno = Console.ReadLine();
+    alunos[i] = aluno;
+
 
 }
 
+for (int i = 0; i < notas.Length; i++)
+{
+    Console.Write($"Digite a nota do {i + 1} aluno: ");
+    double nota = Convert.ToDouble(Console.ReadLine());
+    notas[i] = nota;
+}
+
 Console.WriteLine("\nAlunos :");
-foreach(string nome in alunos)
+foreach (string? nome in alunos)
 {
     Console.WriteLine($"{nome} ");
 }
@@ -19,10 +26,10 @@ foreach(string nome in alunos)
 Console.WriteLine("\nNotas :");
 var somaNotas = 0.0;
 var totalNotas = notas.Count();
-foreach(int nota in notas)
+foreach (int nota in notas)
 {
     somaNotas += nota;
     Console.WriteLine($"{nota}");
 }
 
-Console.WriteLine($"Media aritmetica: {somaNotas/totalNotas}");
+Console.WriteLine($"Media aritmetica: {somaNotas / totalNotas}");
