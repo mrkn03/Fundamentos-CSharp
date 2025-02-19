0ae43db717c668ba01f42ab2e08dd00b3f24429f
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-02-18 23:12:32 -0300

    feat: List parte 1 e arrayList parte 3 e 4

--- Colecoes/ArrayList - Parte 2/Program.cs
+++ Colecoes/ArrayList - Parte 2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System.Collections;
+
+//Adicionar Elementos
+var lista = new ArrayList() { "Maria", 5, true, " ", null };
+
+lista.Add(3.5);
+lista.Insert(2, "Paulo");
+
+//Adicionar coleção de Elementos
+var lista1 = new ArrayList() { "Maria", 5, true };
+
+int[] array1 = { 1, 2, 3 };
+
+lista1.AddRange(array1);
+lista1.InsertRange(2, array1);
--- Colecoes/ArrayList - Parte 4/Program.cs
+++ Colecoes/ArrayList - Parte 4/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+
+var lista = new ArrayList() { "Maria", 5, true, 4.5, null, DateTime.Now };
+
+//Verificar se um elemento existe na colecao
+var res1 = lista.Contains(5);   //true
+bool res2 = lista.Contains("Mari"); //false
+
+Console.WriteLine(res1);
+Console.WriteLine(res2);
+Console.WriteLine(lista.Contains(null)); //true
+
+lista.Sort();   //invalidOperationException
+
+
+var lista1 = new ArrayList() { "Maria", "Ana", "Zilda", "Carlos", "Diná" };
+
+//ArrayList original
+foreach (var item in lista)
+{
+    Console.WriteLine($"{item}");
+}
+
+
+
+//ArrayList ordenado
+lista.Sort();
+
+foreach (var item in lista1)
+{ Console.WriteLine(item); }
+
+
+//Limpar ArrayList
+
+Console.WriteLine($"Numero de elementos no arrayList antes de limpar {lista1.Count}");
+
+lista1.Clear();
+
+Console.WriteLine($"Numero de elementos no arrayList depois de limpar {lista1.Count}");
