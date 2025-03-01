79ee3b783c04e8fd9fb062afcdbb8cc06523f92e
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-01 18:32:51 -0300

    Indexadores

---
+++ Colecoes/Indexadores/Time.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexadores
+{
+    class Time
+    {
+        //Definir os limites do array
+        string[] valor = new string[10];
+
+
+        public string this[int index]
+        {
+            get
+            {
+                if ((index >= 0) && index < valor.Length )
+                {
+                    return valor[index];
+                }
+                return "Erro";
+            }
+
+            set
+            {
+                if ((index >= 0) && index < valor.Length)
+                {
+                    //atribuir o valor
+                    valor[index] = value;
+                }
+            }
+        }
+
+    }
+}
