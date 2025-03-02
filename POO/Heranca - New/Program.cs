0af10fc84a7013959b4eb73226bb5f61f69450e9
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-02 16:51:48 -0300

    Heranca - New

---
+++ POO/Heranca - New/Program.cs
@@ -0,0 +1,21 @@
+﻿Aluno aluno = new();
+aluno.Nome = "Maria";
+aluno.Curso = "Quimica";
+Console.WriteLine(aluno.Saudacao());
+
+Console.ReadKey();
+
+class Pessoa
+{
+    public string? Nome { get; set; }
+
+    public string Saudacao() => $"Oi, Eu sou o(a) {Nome}";
+}
+
+class Aluno : Pessoa
+{
+    public string? Curso { get; set; }
+
+    public new string Saudacao()
+        => $"Oi, Eu sou o(a) {Nome} do curso de : {Curso}.";
+}
