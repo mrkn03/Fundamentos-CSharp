4f9b510d6e021bdf43f9a30804d09b1568eda964
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-03 20:37:45 -0300

    Operadores - is e as

---
+++ POO/Operadores  - is e as/Circulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores____is_e_as
+{
+    class Circulo : Forma
+    {
+        public Circulo()
+        { }
+
+        public Circulo(int x, int y) : base(x, y)
+        { }
+
+        public override void Desenhar()
+        {
+            Console.WriteLine($"Desenhar círculo na posição : ({xpos},{ypos})");
+        }
+
+        public void PintarCirculo()
+        {
+            Console.WriteLine("Pintar o Circulo");
+        }
+    }
+}
---
+++ POO/Operadores  - is e as/Forma.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores____is_e_as
+{
+    class Forma
+    {
+        protected int xpos, ypos;
+
+        public Forma()
+        { }
+
+        public Forma(int x, int y)
+        {
+            xpos = x;
+            ypos = y;
+        }
+
+        public virtual void Desenhar()
+        {
+            Console.WriteLine($"Desenhar - Forma na posição : ({xpos},{ypos})");
+        }
+    }
+}
