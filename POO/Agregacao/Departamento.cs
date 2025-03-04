f4f276cea945d61cacfe605b2cfd65048a397d3a
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-04 18:55:26 -0300

    Agregacao

---
+++ POO/Agregacao/Departamento.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agregacao
+{
+    class Departamento
+    {
+        public string? Nome { get; set; }
+        private List<Professor>? professores = new List<Professor>();
+
+        public Departamento(string? nome)
+        {
+            Nome = nome;
+            professores = new List<Professor>();
+        }
+
+        public void AdicionaProfessor(Professor professor)
+        {
+            professores?.Add(professor);
+        }
+
+        public void ListarProfessores()
+        {
+            Console.WriteLine($"\nDepartamento de : {Nome}");
+            foreach (var professor in professores)
+            {
+                Console.WriteLine($"Nome: {professor.Nome}, Disciplina: {professor.Disciplina}");
+            }
+        }
+    }
+}
---
+++ POO/Agregacao/Program.cs
@@ -0,0 +1,19 @@
+﻿using Agregacao;
+
+Professor prof1 = new Professor("Carlos", "Química");
+Professor prof2 = new Professor("Maria", "Inglês");
+Professor prof3 = new Professor("Ana", "Literatura");
+Professor prof4 = new Professor("Paulo", "Matemátia");
+
+Departamento dep1 = new Departamento("Exatas");
+dep1.AdicionaProfessor(prof1);
+dep1.AdicionaProfessor(prof4);
+dep1.ListarProfessores();
+
+Departamento dep2 = new Departamento("Humanas");
+dep2.AdicionaProfessor(prof2);
+dep2.AdicionaProfessor(prof3);
+dep2.ListarProfessores();
+
+
+Console.ReadKey();
