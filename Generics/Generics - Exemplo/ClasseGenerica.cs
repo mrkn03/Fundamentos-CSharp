f109b8696d218ba5669f1673cae386d0f993939f
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-09 16:22:20 -0300

    Generics - Exemplo

---
+++ Generics/Generics - Exemplo/ClasseGenerica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics___Exemplo
+{
+    class ClasseGenerica<T>
+    {
+        protected T[] obj = new T[5];
+        protected int Contador = 0;
+
+        public void Adicionar(T item)
+        {
+            if (Contador < 5)
+            {
+                obj[Contador] = item;
+            }
+            Contador++;
+        }
+
+
+        //Indexador para a iteração da instrução foreach
+        public T this[int index]
+        {
+            get { return obj[index]; }
+            set { obj[index] = value; }
+        }
+    }
+}
---
+++ Generics/Generics - Exemplo/Program.cs
@@ -0,0 +1,14 @@
+﻿using Generics___Exemplo;
+
+ClasseGenerica<int> objInt = new ClasseGenerica<int>();
+
+objInt.Adicionar(10);
+objInt.Adicionar(20);
+objInt.Adicionar(30);
+objInt.Adicionar(40);
+objInt.Adicionar(50);
+
+for(int i =0; i<5; i++)
+{
+    Console.WriteLine(objInt[i]);
+}
