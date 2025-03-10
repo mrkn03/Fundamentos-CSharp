f22043acf6c12e465dc59f5f225324f86815cebc
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-09 22:37:30 -0300

    Dictionary - Exemplo

---
+++ Generics/Dictionary - Exemplo/Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary___Exemplo
+{
+    class Aluno
+    {
+        protected string? Nome { get; set; }
+        protected int Nota { get; set; }
+
+        public Aluno(string nome, int nota)
+        {
+            Nome = nome;
+            Nota = nota;
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public double GetNota()
+        {
+            return Nota;
+        }
+
+        public void SetNota(int nota)
+        {
+            Nota = nota;
+        }
+
+    }
+}
---
+++ Generics/Dictionary - Exemplo/Program.cs
@@ -0,0 +1,93 @@
+﻿using Dictionary___Exemplo;
+
+Console.WriteLine("### Alunos e Notas ###\n");
+
+var alunos = new Dictionary<int, Aluno>()
+{
+    { 1, new Aluno("Maria",7) },
+    { 2, new Aluno("Eric", 8) },
+    { 3, new Aluno("Ana", 9 ) },
+    { 4, new Aluno("Alex", 6) },
+    { 5, new Aluno("Diná", 5) }
+};
+
+ExibirColecao(alunos);
+do
+{
+    Console.WriteLine("\nInforme o código do Aluno a localizar (99 sai)");
+    int codigo = Convert.ToInt32(Console.ReadLine());
+
+    if (codigo == 99)
+        break;
+
+    var resultado = alunos.ContainsKey(codigo);
+    if (resultado)
+    {
+        Console.WriteLine("Informe a nota (1 a 10)");
+        var nota = Convert.ToInt32(Console.ReadLine());
+        alunos[codigo].SetNota(nota);
+    }
+    else
+    {
+        Console.WriteLine("Aluno não localizado");
+    }
+}
+while (true);
+ExibirColecao(alunos);
+
+Console.WriteLine("\nInforme o código do Aluno a remover");
+int cod = Convert.ToInt32(Console.ReadLine());
+if (alunos.ContainsKey(cod))
+{
+    alunos.Remove(cod);
+    Console.WriteLine("\nAluno removido com sucesso");
+}
+else
+{
+    Console.WriteLine("\nCódigo não encontrado");
+}
+ExibirColecao(alunos);
+
+Console.WriteLine("\n### Incluindo um novo Aluno ###");
+Console.WriteLine("\nInforme o nome do aluno");
+string novoNome = Console.ReadLine();
+
+Console.WriteLine("Informe a nota do aluno");
+int novaNota = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o código do aluno");
+int novoCodigo = Convert.ToInt32(Console.ReadLine());
+
+
+if (!alunos.ContainsKey(novoCodigo))
+{
+    alunos.Add(novoCodigo, new Aluno(novoNome, novaNota));
+    Console.WriteLine("Aluno incluido com sucesso");
+}
+else
+{
+    Console.WriteLine("\nCódigo já existe encontrado");
+}
+ExibirColecao(alunos);
+
+Console.WriteLine("\nAlunos ordenados por nome");
+
+var alunosOrdenados = alunos.OrderBy(x => x.Value.GetNome());
+foreach (var item in alunosOrdenados)
+{
+    Console.WriteLine($"{item.Key} {item.Value.GetNome()} {item.Value.GetNota()}");
+}
+
+alunos.Clear();
+ExibirColecao(alunos);
+Console.WriteLine("Fim do exercício");
+
+Console.ReadKey();
+
+static void ExibirColecao(Dictionary<int, Aluno> alunos)
+{
+    foreach (var item in alunos)
+    {
+        Console.WriteLine($"{item.Key} {item.Value.GetNome()} {item.Value.GetNota()}");
+    }
+}
