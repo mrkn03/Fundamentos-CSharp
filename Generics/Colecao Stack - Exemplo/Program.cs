1c97151313a95119b20d8855907fbf55b5e5cf37
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-14 19:59:22 -0300

    Colecao Stack - Exemplo

---
+++ Generics/Colecao Stack - Exemplo/Program.cs
@@ -0,0 +1,22 @@
+﻿using Colecao_Stack___Exemplo;
+
+Console.WriteLine("\n### Stack - Undo ###");
+
+EditorTexto editor = new EditorTexto();
+Console.WriteLine("\nDigitando...");
+
+editor.DigitarChar('S');
+editor.DigitarChar('t');
+editor.DigitarChar('a');
+editor.DigitarChar('c');
+editor.DigitarChar('q');
+
+Console.WriteLine("\nFazendo o Undo...");
+editor.Undo();
+editor.Undo();
+
+Console.WriteLine("\nRedigitando...");
+
+editor.DigitarChar('c');
+editor.DigitarChar('k');
+editor.DigitarChar('!');
