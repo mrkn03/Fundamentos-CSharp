a389aa03a9c02ed57ab3d529ec12ffb7bade83e2
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-13 21:52:15 -0300

    Colecoes Set - SortedSet

---
+++ Generics/Colecoes Set - SortedSet/Program.cs
@@ -0,0 +1,32 @@
+﻿var numeros = new SortedSet<int>() { 1, 2, 3, 4, 5 };
+ExibirColecao(numeros);
+
+var frutas = new SortedSet<string>() { "maçã", "banana", "laranja" };
+ExibirColecao(frutas);
+
+var pares = new SortedSet<int>();
+for (int i  = 6; i  > 0 ; i --)
+{
+    pares.Add(i * 2);
+}
+ExibirColecao(pares);
+
+var lista1 = new List<int>() { 1, 2, 3, 4, 5 };
+var conunto = new SortedSet<int>(lista1);
+ExibirColecao(conunto);
+
+var frutas1 = new List<string> { "uva", "banana", "pera", "manga" };
+SortedSet<string> minhasFrutas = new SortedSet<string>(frutas);
+ExibirColecao(minhasFrutas);
+
+var impares = new int[] { 1, 13, 7, 9, 11, 13, 5 };
+SortedSet<int> meusNumeros = new SortedSet<int>(impares);
+ExibirColecao(meusNumeros);
+
+static void ExibirColecao<T>(IEnumerable<T> colecao)
+{
+    foreach (var item in colecao)
+    {
+        Console.WriteLine(item);
+    }
+}
