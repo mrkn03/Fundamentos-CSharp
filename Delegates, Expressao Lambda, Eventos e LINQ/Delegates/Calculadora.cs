1fbb02567fdd39cdc69a665fd70df1f0903c2e1d
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-18 19:47:41 -0300

    Delegates

---
+++ Delegates, Expressao Lambda, Eventos e LINQ/Delegates/Calculadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Calculadora
+    {
+        public static float Somar(float n1, float n2)
+        {
+            return n1 + n2;
+        }
+
+        public static float Multiplicar(float n1, float n2)
+        {
+            return n1 * n2;
+        }
+
+        public static float Dividir(float n1, float n2)
+        {
+            return n1 / n2;
+        }
+
+        public static float Subtrair(float n1, float n2)
+        {
+            return n1 - n2;
+        }
+    }
+}
