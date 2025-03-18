d66c2e7128a3d1c0d3a8642050fc3ee72285800e
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-18 19:55:22 -0300

    Delegate Multicast

---
+++ Delegates, Expressao Lambda, Eventos e LINQ/Delegate Multicast/Program.cs
@@ -0,0 +1,25 @@
+﻿
+MeuDelegate meuDelegate = new MeuDelegate(Metodo1);
+meuDelegate += Metodo2;
+meuDelegate += Metodo3;
+
+meuDelegate("Olá Mundo");
+
+
+static void Metodo1(string mensagem)
+{
+    Console.WriteLine("Metodo 1:" + mensagem);
+}
+
+static void Metodo2(string mensagem)
+{
+    Console.WriteLine("Metodo 2:" + mensagem);
+}
+
+static void Metodo3(string mensagem)
+{
+    Console.WriteLine("Metodo 3:" + mensagem);
+}
+
+
+public delegate void MeuDelegate(string mensagem);
