1c6c05058595c292bc16b1c4e6d8e21ac7829fbc
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-18 21:48:52 -0300

    Delegates - Function

---
+++ Delegates, Expressao Lambda, Eventos e LINQ/Delegates - Function/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Informe um numero flutuante: ");
+double numero = Convert.ToDouble(Console.ReadLine());
+
+
+Func<double, double> raizQuadrada = (x => Math.Sqrt(x));
+
+Console.WriteLine($"A raiz quadrada de {numero} é {raizQuadrada(numero)}");
