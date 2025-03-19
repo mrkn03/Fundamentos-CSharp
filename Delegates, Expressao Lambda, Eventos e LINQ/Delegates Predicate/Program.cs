562bcdb60cb6f83dffd5e8c833098d5a4c5227b4
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-18 21:20:40 -0300

    Delegates - Predicate

---
+++ Delegates, Expressao Lambda, Eventos e LINQ/Delegates Predicate/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Security;
+
+Console.WriteLine("Informe um numero inteiro: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+Predicate<int> verificaPar =  (x => x % 2 == 0);
+
+if (verificaPar(numero))
+    Console.WriteLine($"O numero {numero} é PAR");
+else
+    Console.WriteLine($"O numero {numero} é IMPAR");
