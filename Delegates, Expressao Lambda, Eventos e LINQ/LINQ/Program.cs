c9be5818c5d3e4a15cb5f586f7e99aad284802fe
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-20 23:00:44 -0300

    LINQ

---
+++ Delegates, Expressao Lambda, Eventos e LINQ/LINQ/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//Usando sintaxe de consulta
+List<string> nomes = new List<string>() { "Ana", "Maria", "Pedro", "Lair", "Hugo" };
+
+var resultado = from nome in nomes
+                where nome.Contains('o')
+                select nome;
+
+ExibirResultado(resultado);
+
+
+//Usando sintaxe de métodos
+var resultado1 = nomes.Where(nome => nome.Contains('a'));
+ExibirResultado(resultado1);
+
+
+static void ExibirResultado(IEnumerable<string> resultado)
+{
+    Console.WriteLine();
+    foreach (var item in resultado)
+    {
+
+        Console.WriteLine(item);
+    }
+}
