a1e2cea3a31db288f2571abc202525ff56954901
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-22 00:36:19 -0300

    LINQ - Consultas III

---
+++ Delegates, Expressao Lambda, Eventos e LINQ/LINQ - Consultas III/Program.cs
@@ -0,0 +1,22 @@
+﻿using LINQ___Consultas_III;
+
+var listaProdutos = Produto.GetProdutos();
+
+var produtosPorCategoria = listaProdutos
+                            .GroupBy(p => p.Categoria)
+                            .OrderBy(c => c.Key)
+                            .Select(g => new
+                            {
+                                Categoria = g.Key,
+                                Produtos = g.OrderBy(p => p.Nome).Select(p => new {Nome = p.Nome, Preco = p.Preco, Estoque = p.Estoque})
+                            });
+
+foreach(var grupo in produtosPorCategoria)
+{
+    Console.WriteLine(grupo.Categoria);
+
+    foreach(var produto in grupo.Produtos)
+    {
+        Console.WriteLine($"\t{produto.Nome} \t {produto.Preco:C2}  \t{produto.Estoque}");
+    }
+}
