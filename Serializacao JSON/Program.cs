92db127d45020d93c44b604a4ad3b9790bcb8149
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-23 15:00:11 -0300

    Serializacao JSON

---
+++ Serializacao JSON/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Text.Json;
+using Serializacao_JSON;
+
+Aluno aluno1 = new Aluno(10, "Maria", "maria@yahoo.com", 17);
+
+var caminhoArquivo = @"d:\dados\Serializados\AlunoSerializado.json";
+
+using (FileStream stream = new FileStream(caminhoArquivo,
+                                          FileMode.OpenOrCreate, FileAccess.ReadWrite))
+{
+    JsonSerializer.Serialize(stream, aluno1);
+}
+
+Console.WriteLine("Objeto serializado para JSON com sucesso!");
