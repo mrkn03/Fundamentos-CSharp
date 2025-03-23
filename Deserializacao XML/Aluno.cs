0210e65b6512e466dec8ad0f17b4a1acc6064721
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-23 14:55:45 -0300

    Desserializacao XML

---
+++ Deserializacao XML/Aluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deserializacao_XML
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int Idade;
+
+        public Aluno()
+        {
+        }
+        public Aluno(int id, string name, string email, int idade)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Idade = idade;
+        }
+    }
+}
---
+++ Deserializacao XML/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+using Deserializacao_XML;
+
+string caminhoArquivo = @"d:\dados\Serializados\AlunoSerializado.xml";
+
+XmlSerializer serializer = new XmlSerializer(typeof(Aluno));
+
+using (StreamReader reader = new StreamReader(caminhoArquivo))
+{
+    var aluno = (Aluno)serializer.Deserialize(reader);
+
+    Console.WriteLine($"Aluno XML desserializado - Id: {aluno.Id}, Nome: {aluno.Name}, " +
+                      $"Email: {aluno.Email}, Idade: {aluno.Idade}");
+}
