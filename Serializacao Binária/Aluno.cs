74eace7023c181d751258da4a74d1c58a73d3280
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-23 14:00:30 -0300

    Serializacao Binaria

---
+++ Serializacao Binária/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacao_Binária
+{
+    [Serializable]
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        [NonSerialized]
+        public int Idade;
+
+        public Aluno(int id, string nome, string email, int idade)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Idade = idade;
+        }
+    }
+}
---
+++ Serializacao Binária/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serializacao_Binária
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno1 = new Aluno(101, "Maria", "maria@yahoo.com", 17);
+            var caminhoArquivo = @"d:\dados\Serializados\AlunoSerializado.bin";
+
+            using (FileStream stream = new FileStream(caminhoArquivo,
+                                            FileMode.OpenOrCreate,
+                                            FileAccess.ReadWrite))
+            {
+#pragma warning disable SYSLIB0011
+                var bf = new BinaryFormatter();
+                bf.Serialize(stream, aluno1);
+#pragma warning disable SYSLIB0011
+                Console.WriteLine("Tecle algo para realizar a Desserialização");
+                Console.ReadKey();
+                stream.Seek(0, SeekOrigin.Begin);
+                var alunoDesserializado = (Aluno)bf.Deserialize(stream);
+                Console.WriteLine(alunoDesserializado.Nome);
+            }
+
+            Console.WriteLine("Serialização binária concluída");
+            Console.ReadKey();
+        }
+    }
+}
