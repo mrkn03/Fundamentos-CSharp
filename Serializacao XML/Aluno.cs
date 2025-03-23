9e0b094ef62ec08df473d47a4c237b4ec9c68b45
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-23 14:52:32 -0300

    Serializacao XML

---
+++ Serializacao XML/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializacao_XML
+{
+    class Aluno
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public int Idade;
+
+        public Aluno()
+        {
+
+        }
+
+        public Aluno(int id, string nome, string email, int idade)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Idade = idade;
+        }
+
+    }
+}
---
+++ Serializacao XML/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Serialization;
+using Serializacao_XML;
+
+Aluno aluno1 = new Aluno(101, "Maria", "maria@yahoo.com", 17);
+
+string caminhoArquivo = @"d:\dados\Serializados\AlunoSerializado.xml";
+
+XmlSerializer serializer = new XmlSerializer(typeof(Aluno));
+
+using (StreamWriter writer = new StreamWriter(caminhoArquivo))
+{
+    serializer.Serialize(writer, aluno1);
+}
+Console.WriteLine("Objeto serializado para XML com sucesso!");
+Console.ReadKey();
