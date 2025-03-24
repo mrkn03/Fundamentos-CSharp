fe3812772e9221d201a013e8e37ad030e4589100
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-23 23:32:31 -0300

    Tratamento de excecoes I

---
+++ Tratamento de Excecoes/Program.cs
@@ -0,0 +1,21 @@
+﻿await LancaExceptionAsync();
+
+Console.ReadKey();
+
+static async Task LancaExceptionAsync()
+{
+    try
+    {
+        var primeiraTask = Task.Run(() => {
+            Task.Delay(1000);
+            throw new IndexOutOfRangeException
+            ("IndexOutOfRangeException lançada explicitamente.");
+        });
+        await primeiraTask;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Bloco catch executado");
+        Console.WriteLine(ex.Message);
+    }
+}
---
+++ Tratamento de Excecoes/TesteAsync.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tratamento_de_Excecoes
+{
+    class TesteAsync
+    {
+        public Task MinhaTarefaAsync()
+        {
+            return Task.Run(() => {
+                Task.Delay(2000);
+                throw new Exception("Minha Exception...");
+            });
+        }
+        public async void ChamaTarefaAsync()
+        {
+            await MinhaTarefaAsync();
+        }
+    }
+}
