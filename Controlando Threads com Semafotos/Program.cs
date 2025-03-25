bd626e7632a76c5a137b62bfac7a6eacdedc34fb
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-24 23:45:37 -0300

    Controlando Threads com Semafotos

---
+++ Controlando Threads com Semafotos/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    public static Semaphore threadPool = new Semaphore(3, 5);
+    static void Main(string[] args)
+    {
+        for(int i =0 ; i < 10; i++)
+        {
+            Thread threadObject = new Thread(new ThreadStart(ProcessarOperacao));
+
+            threadObject.Name = $"Thread {i}";
+            threadObject.Start();
+
+        }
+    }
+
+    private static void ProcessarOperacao()
+    {
+        Console.WriteLine($"Thread {Thread.CurrentThread.Name}" + 
+            $"Entrou na sessao critica...");
+
+        Thread.Sleep(6000);
+
+        Console.WriteLine($"Thread {Thread.CurrentThread.Name}" +
+            $"Foi liberada...");
+    }
+}
