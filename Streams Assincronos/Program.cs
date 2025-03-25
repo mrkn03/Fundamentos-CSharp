cafca66dc3b89fae13d4fe2789e47f5e1062e2ed
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-03-24 23:27:59 -0300

    Streams Assincronos

---
+++ Streams Assincronos/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        //var meses = new List<string>() { "Janeiro", "Fevereiro", "Março", "Abril" };
+        await foreach(var mes in GeraMeses())
+        {
+            Console.WriteLine(mes);
+        }
+    }
+
+    static private async IAsyncEnumerable<string> GeraMeses()
+    {
+        yield return "Janeiro";
+        yield return "Fevereiro";
+        await Task.Delay(1000);
+        yield return "Março";
+        yield return "Abril";
+    }
+}
